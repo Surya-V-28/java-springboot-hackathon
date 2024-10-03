4dc8a41177da5bb63ac1188bd9a2bc814dcef0b8
Thina45 <thinasharma1@gmail.com>
2024-10-04 01:27:57 +0530

    Added login form
    here i added login form functionality

---
+++ java-springboot-hackathon-main/src/main/java/com/codeathonJava/codeathonJava/model/MyAppUser.java
@@ -0,0 +1,53 @@
+package com.codeathonJava.codeathonJava.model;
+
+
+
+public class MyAppUser {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+
+    // Getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "MyAppUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
